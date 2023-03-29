bc7772c41d08db742cb61f7e1c372fc99a5ca506
hrdipto <jhondipto@gmail.com>
2023-03-30 03:51:29 +0600

    initial update

---
+++ kizuma/src/components/navigation/navigation.js
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+import logo from "../../../public/logo.png";
+
+const Navigation = () => {
+    <>
+            <Image src={logo} />
+    </>
+}
+
+export default Navigation;
